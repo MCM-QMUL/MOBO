dd0a2a45a4972ff81a2b65c2e4a67fb867eda276
Mechanics of Composite Materials Group <105063133+MCM-QMUL@users.noreply.github.com>
2025-03-25 11:11:51 +0000

    Update MOBO_methods.py

--- src/MOBO_methods.py
+++ src/MOBO_methods.py
@@ -1,3 +1,16 @@
+# Open-source Code for Multi-objective Bayesian Optimisation of Spinodoid Cellular Materials
+# 
+# If using this code for research or industrial purposes, please cite:
+# Kansara, H., Khosroshahi, S.F., Guo, L., Bessa, M.A. and Tan, W., 2025. 
+# Multi-objective Bayesian Optimisation of Spinodoid Cellular Structures for Crush Energy Absorption
+# Computer Methods for Applied Mechanics and Engineering, 117890
+# DOI: https://doi.org/10.1016/j.cma.2025.117890
+# Preprint: https://arxiv.org/abs/2411.14508
+#
+# Author: Wei Tan (wei.tan@qmul.ac.uk)
+# Queen Mary University of London
+#
+
 import torch
 from botorch.utils.transforms import normalize, unnormalize
 from botorch.utils.multi_objective.box_decompositions.non_dominated import FastNondominatedPartitioning
