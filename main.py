3df67c1065839e6c6cd6ad60e91b14dd44036604
hmk1431 <105068734+hmk1431@users.noreply.github.com>
2025-03-26 19:27:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -34,15 +34,3 @@ def main(config):
 if __name__ == "__main__":
     main()
 
-'''
-TO DO:
-- break down the process into multiple different stages with different files.
-- before optimisation begins, must create a csv file containing the initial data in the same folder as the output folder created by hydra
-- then in each step of optimisation when batch_size > 1, all the inputs should be stored in the csv file containing the initial data.
-- then when the jobid is greater than 1, the main_local() file should then call the spinodoid script to evaluate the inputs with different nodes, 
-each running a different simulation.
-- this data should then be stored within the initial csv file.
-
-NEED TO FIGURE OUT:
-- how to call different nodes based on jobids.
-'''
